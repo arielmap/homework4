7895476d83fff0d9ab33bf9cbc16c76e2eddd710
Vladislav izmailov <arielmap@icloud.com>
2022-08-20 02:39:52 +0300

    jomework27

---
+++ homework27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    sum = sum + Convert.ToInt32(number[i].ToString());
+}
+Console.WriteLine("Сумма цифр в числе = ");
+Console.WriteLine(sum);
