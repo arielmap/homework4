1734c4a470034c5c69732f97f65402dcdce15a8b
Vladislav izmailov <arielmap@icloud.com>
2022-08-20 02:34:54 +0300

    homework25

---
+++ homework25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число А ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < number2; i++)
+{
+    result = result * number1;
+}
+Console.WriteLine("Число А в степени В = ");
+Console.WriteLine(result);
