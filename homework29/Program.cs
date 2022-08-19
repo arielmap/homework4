e8a47f01dcd35311f0aa71825911fad34fa8089d
Vladislav izmailov <arielmap@icloud.com>
2022-08-20 02:56:02 +0300

    homework29

---
+++ homework29/Program.cs
@@ -0,0 +1,20 @@
+﻿string number = Console.ReadLine();
+string[] array = new string[number.Length];
+int index = 0;
+foreach (var item in number)
+{
+    if (item != ',')
+    {
+        array[index] += item.ToString();
+    }
+    else
+    {
+        index++;
+    }
+}
+Console.Write("[ ");
+for (int i = 0; i <= index - 1; i++)
+{
+    Console.Write(array[i] + ",");
+}
+Console.Write(array[index] + " ]");
